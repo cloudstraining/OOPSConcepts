d7ed468a88e220e8c3d33186f73a47032c062025
Cloudstraining <cloudstraining@outlook.com>
2018-06-08 19:38:25 +1000

    7. Nested Classes

---
+++ 7.NestedClasses/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.NestedClasses
+{   /// <summary>
+    /// OOPS concepts - Object oriented programming concepts training program from Clouds.training
+    /// 7.Nested Classes
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Container.Nested nestedInstance = new Container.Nested();
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+
+    class Container
+    {
+        public class Nested
+        {
+            // Add code here.  
+            public Nested()
+            {
+                Console.WriteLine("Am from nested class");
+            }
+            
+        }
+    }
+
+}
