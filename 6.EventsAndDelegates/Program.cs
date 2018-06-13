e04ecf3047de036dcaebb0eb5919949ca9259a33
Cloudstraining <cloudstraining@outlook.com>
2018-06-13 14:02:12 +1000

    6.Events and Delegates project added

---
+++ 6.EventsAndDelegates/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.EventsAndDelegates
+{  /// <summary>
+   /// OOPS concepts - Object oriented programming concepts training program from Clouds.training
+   /// 6.Events And Delegates
+   /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter c = new Counter(5);
+            c.ThresholdReached += c_ThresholdReached;
+
+            Console.WriteLine("press 'Enter' key to increase total");
+            while (Console.ReadKey(true).KeyChar == 13)
+            {
+                Console.WriteLine("adding one");
+                c.Add(1);
+            }
+        }
+
+        static void c_ThresholdReached(object sender, ThresholdReachedEventArgs e)
+        {
+            Console.WriteLine("The threshold of {0} was reached at {1}.", e.Threshold, e.TimeReached);
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+    
+    class Counter
+    {
+        private int threshold;
+        private int total;
+
+        public Counter(int passedThreshold)
+        {
+            threshold = passedThreshold;
+        }
+
+        public void Add(int x)
+        {
+            total += x;
+            if (total >= threshold)
+            {
+                ThresholdReachedEventArgs args = new ThresholdReachedEventArgs();
+                args.Threshold = threshold;
+                args.TimeReached = DateTime.Now;
+                OnThresholdReached(args);
+            }
+        }
+
+        protected virtual void OnThresholdReached(ThresholdReachedEventArgs e)
+        {
+            EventHandler<ThresholdReachedEventArgs> handler = ThresholdReached;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
+    }
+
+    public class ThresholdReachedEventArgs : EventArgs
+    {
+        public int Threshold { get; set; }
+        public DateTime TimeReached { get; set; }
+    }
+
+   // public delegate void ThresholdReachedEventHandler(object sender, ThresholdReachedEventArgs e);
+}
---
+++ 8.AccessModifiersAndAccessLevels/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.AccessModifiersAndAccessLevels
+{
+    /// <summary>
+    /// OOPS concepts - Object oriented programming concepts training program from Clouds.training
+    /// 8. Access Modifiers And AccessLevels
+    /// public 
+    /// private
+    /// protected  
+    /// internal
+    /// protected internal
+    /// private protected
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region public
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+            #endregion
+ 
+
+        }
+    }
+
+    #region public
+
+
+    #endregion
+
+    
+
+
+}
