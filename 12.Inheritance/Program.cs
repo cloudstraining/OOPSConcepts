344147f0d8aabf49b86c6bb60c1092a0c56a2351
Cloudstraining <cloudstraining@outlook.com>
2018-06-19 12:40:21 +1000

    12. Inheritance 13. Interfaces

---
+++ 12.Inheritance/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Inheritance
+{
+
+    /// <summary>
+    /// OOPS concepts - Object oriented programming concepts training program from Clouds.training
+    /// 12.Inheritance
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //AdminUser obj = new AdminUser();
+            SuperAdminUser obj = new SuperAdminUser();
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+    public class Users
+    {
+        public virtual void AddUser(int i)
+        {
+            // abstract example
+        }
+
+
+        public virtual void UpdateUser(int i)
+        {
+            // sealed example
+        }
+    }
+
+    public abstract class AdminUser : Users
+    {
+        public abstract override void AddUser(int i);
+
+        public sealed override void UpdateUser(int i) {
+            //implementation
+        }
+    }
+
+    public class SuperAdminUser : AdminUser
+    {
+        public override void AddUser(int i)
+        {
+            // New implementation.
+        }
+
+        //public abstract override void AddUser1(int i);
+
+        //public override void UpdateUser(int i)
+        //{
+        //    // New implementation.
+        //}
+
+    }
+
+
+    //Basic example 1
+    //public sealed class SealedClass{};
+
+    //public abstract class BaseClass1 { }
+
+    //public abstract class BaseClass { }
+
+    //public class DerivedClass : BaseClass { }
+
+
+
+
+}
---
+++ 13.Interfaces/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Interfaces
+{
+    /// <summary>
+    /// OOPS concepts - Object oriented programming concepts training program from Clouds.training
+    /// 13.Interfaces
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+
+            obj.i = 1;
+            obj.j = 2;
+
+            Console.WriteLine("Sum from class " + obj.Sum(obj));
+            Console.WriteLine("Multiplication from class " + obj.Multiplication(obj));
+
+
+            MyStruct str;
+            str.i = 10;
+            str.j = 20;
+            
+
+            Console.WriteLine("Sum from structure " + str.Sum(str));
+            Console.WriteLine("Multiplication from structure " + str.Multiplication(str));
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+
+
+    interface IMaths<T>
+    {
+         int Sum(T obj);
+
+        int Multiplication(T obj);
+    }
+
+    //static class MyStaticClass: IMaths<MyStaticClass>
+    //{
+    //}
+    class MyClass : IMaths<MyClass>
+    {
+        public int i { get; set; }
+        public int j { get; set; }
+
+        public int Multiplication(MyClass obj)
+        {
+            return obj.i * obj.j;
+
+        }
+
+        public int Sum(MyClass obj)
+        {
+            return obj.i + obj.j;
+        }
+    }
+
+    struct MyStruct : IMaths<MyStruct>
+    {
+        public int i;
+        public int j;
+
+        public int Multiplication(MyStruct obj)
+        {
+            return obj.i * obj.j;
+
+        }
+
+        public int Sum(MyStruct obj)
+        {
+            return obj.i + obj.j;
+        }
+    }
+}
